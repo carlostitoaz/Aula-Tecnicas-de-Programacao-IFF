7c1f1fdf062a303ff174524cbe91c7692b861968
Carlos Henrique Tito Azeredo <58192819+carlostitoaz@users.noreply.github.com>
2023-08-12 15:17:47 -0300

    Update ListaEncadeada.java

--- Lista_1/ListaEncadeada.java
+++ Lista_1/ListaEncadeada.java
@@ -1,1 +1,15 @@
-
+public interface ListaEncadeada{
+    
+    public void adicionarInicio(int dado);
+    public void adicionarFim(int dado);
+    public boolean adicionarMeio(int posicaoInclusao, int dado);
+    public boolean excluirInicio();
+    public boolean excluirFim();
+    public boolean excluirMeio(int posicao);
+    public int obterDado(int posicao);
+    public int obterTotalDeElementos();
+    
+    /*Iterador obterIterador();*/
+    @Override
+    public String toString();
+}
